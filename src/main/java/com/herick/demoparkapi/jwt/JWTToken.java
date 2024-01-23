f80ad56a1930f7e96017fe3ce4f05fd80a1a78eb
Herick Pereira <42222113+herickkgb@users.noreply.github.com>
2024-01-22 22:35:36 -0300

    implementando a classe JWT e suas dependencias

---
+++ src/main/java/com/herick/demoparkapi/jwt/JWTToken.java
@@ -0,0 +1,14 @@
+package com.herick.demoparkapi.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class JWTToken {
+    private String token;
+}
